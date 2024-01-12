e9e551b1f25f37014cb071f42ebd3e450394c25b
Sanjay-Ramesh <sanjayramesh2111@gmail.com>
2024-01-12 12:34:47 +0530

    Created csv file as datas and modified contact us selection box

--- Pages/Contact_Us.py
+++ Pages/Contact_Us.py
@@ -1,11 +1,14 @@
 import streamlit as st
 from send_email import send_email
+import pandas
 
 st.header("Contact Us")
 
+df = pandas.read_csv("topics.csv")
+
 with st.form(key="email_forum"):
     user_email = st.text_input("Your Email Address")
-    selectionbox = st.selectbox("Select an Option", ["Job Inquires", "Project Proposals", "Others"])
+    selectionbox = st.selectbox("Select an Option", df["title"])
     row_message = st.text_area("Your Message")
     message = f"""\
 Subject: New Email from {user_email}
